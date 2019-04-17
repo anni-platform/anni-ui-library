c848b3096d2d033c748560273944ce9fe377eb60
shiftsave <ivan.cruz@gmail.com>
2019-04-17 13:52:37 -0500

    small changes

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -12,20 +12,24 @@ export const OutlineIcon = styled(Icons)`
     props.color ? `${props.theme.colorPrimary}` : `${props.theme.iconPrimary}`};
   stroke-width: ${props =>
     props.strokeWidth ? `${props.strokeWidth}px` : '2px'};
+
   &#logo {
     stroke: ${({ theme }) => theme.colorPrimary};
     height: 50px;
     stroke-width: 1;
     width: 50px;
+
     > :nth-child(4) {
       fill: ${({ theme }) => theme.colorPrimary};
       stroke: none;
     }
+
     > :nth-child(5) {
       fill: ${({ theme }) => theme.colorPrimary};
       stroke: none;
     }
   }
+
   &#more {
     > :nth-child(2) {
       stroke: ${props =>
@@ -34,12 +38,14 @@ export const OutlineIcon = styled(Icons)`
           : `${props.theme.iconPrimary}`};
       stroke-width: 3;
     }
+
     > :nth-child(3),
     > :nth-child(4) {
       stroke: ${({ theme }) => theme.iconPrimary};
       stroke-width: 3;
     }
   }
+
   &#view {
     > :nth-child(2) {
       fill: ${({ theme }) => theme.iconPrimary};
